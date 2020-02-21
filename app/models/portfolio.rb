3c4276a75dfbec7d0c1d611a3c09b4e700ca0c37
Topwind13 <wind13wind@gmail.com>
2020-02-21 18:32:24 +0700

    Implemented set defaults images for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,17 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  def self.ios
+  	where(subtitle: 'IOS')
+  end
+
+  scope :ror_items, -> {where(subtitle: 'Ruby On Rails')}
+
+
+  after_initialize :set_defaults
+
+  def set_defaults
+  	self.main_image ||= "https://via.placeholder.com/600x400"
+  	self.thumb_image ||= "https://via.placeholder.com/300x200"
+  end
 end
